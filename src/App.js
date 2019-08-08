23110122f9c8c14ce058384122b874766e4d6ce7
manuelsilv <manuel-silva@live.nl>
2019-08-08 10:34:19 +0200

    portfoliorouting

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+ 
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+import Contact from './components/pages/Contact';
+import Error from './components/Error';
+import Nav from './components/Nav';
+import Footer from './components/Footer'
+
 import './App.css';
 
-function App() {
+
+class App extends Component {
+  render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <BrowserRouter>
+        <div>
+          <Nav />
+            <Switch>
+             <Route exact path="/" component={Home}/>
+             <Route path="/about" component={About}/>
+             <Route path="/contact" component={Contact}/>
+            <Route component={Error}/>
+            </Switch>
+          <Footer/>
+        </div> 
+      </BrowserRouter>
+
     </div>
   );
+  }
 }
 
 export default App;
---
+++ src/components/pages/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+ 
+const Contact = () => {
+    return (
+       <div>
+          <h1>Contact US</h1>
+          <p>Contact US </p>
+       </div>
+    );
+}
+ 
+export default Contact;
